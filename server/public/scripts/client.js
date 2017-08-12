0bf4964956c51765061f035d21a67c24f87380d8
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-12 17:20:33 -0500

    ajax get call setup for getlist, database framework next

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,18 +4,25 @@ $('document').ready(function(){
 
     $('#addToList').on('click', function(){
         console.log('add button was clicked');
-        $('#toDoRows').append('<tr><td>Get the button to work</td></tr>');
+        //$('#toDoRows').append('<tr><td>Get the button to work</td></tr>');//testing first button click
         var toDoText = $('#toDoTask').val();
         $.ajax({
             method: 'POST',
             url: '/toDoListRoute',
             success: function(response){
                 console.log('post route hit');
+                getList();
             }
         })
     });
 });
 
 function getList(){
-
+    $.ajax({
+        type: 'GET',
+        url: '/toDoListRoute',
+        success: function getSuccessCallback(response){
+            console.log(response);
+        }
+    })
 }
