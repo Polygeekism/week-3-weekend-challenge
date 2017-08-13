a1668732da077d1e57f0f18e27a2d4d8b6c28270
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-13 17:01:43 -0500

    database rows printed to DOM on load

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,15 @@
 
 
-$('document').ready(function(){
+$('document').ready(function () {
     getList();
-    $('#addToList').on('click', function(){
+    $('#addToList').on('click', function () {
         console.log('add button was clicked');
         //$('#toDoRows').append('<tr><td>Get the button to work</td></tr>');//testing first button click
         var toDoText = $('#toDoTask').val();
         $.ajax({
             method: 'POST',
             url: '/toDoListRoute',
-            success: function(response){
+            success: function (response) {
                 console.log('post route hit');
                 getList();
             }
@@ -17,12 +17,21 @@ $('document').ready(function(){
     });
 });
 
-function getList(){
+function getList() {
     $.ajax({
         type: 'GET',
         url: '/toDoListRoute',
-        success: function getSuccessCallback(response){
+        success: function getSuccessCallback(response) {
             console.log(response);
+            printList(response);
         }
     })
 }
+
+function printList(dataRows) {
+    for (var i = 0; i < dataRows.length; i++) {
+        var task = dataRows[i];
+        $('#listOfTasks').prepend('<tr><td>' + task.task_description + '</td><td>' + task.task_complete + '</td></tr>');
+    }
+}
+
