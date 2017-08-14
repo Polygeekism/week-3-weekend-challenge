04c24b4b130ac245cbeb0dbfc79258b4bec49e44
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-14 11:00:34 -0500

    getting some styling to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -35,6 +35,7 @@ $('document').ready(function () {
             success: function (response) {
                 //console.log('post route hit');
                 getList();
+                $('#toDoTask').val('');
             }
         })
     });
